c2cee56821ed00437ac4abb591d057c114b6e21c
Nick Hellemans <NickHellemans93@gmail.com>
2022-07-25 13:02:49 +0200

    feat(story): add patient id to summary

--- pages/stories/create-patient/index.tsx
+++ pages/stories/create-patient/index.tsx
@@ -66,6 +66,14 @@ export default function CreatePatientStory() {
           <div className="mt-5 border-t border-gray-200">
             <dl className="sm:divide-y sm:divide-gray-200">
               <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+                <dt className="text-sm font-medium text-gray-500">
+                  Patient id
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {createdPatient.id}
+                </dd>
+              </div>
+              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
                 <dt className="text-sm font-medium text-gray-500">Full name</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {createdPatient.profile?.first_name ||
