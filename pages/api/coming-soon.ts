e703d466415f16a0e0df97d097009f2f2c078010
Nick Hellemans <NickHellemans93@gmail.com>
2022-08-11 21:49:00 +0200

    fix: fetch

--- pages/api/coming-soon.ts
+++ pages/api/coming-soon.ts
@@ -8,28 +8,27 @@ export default async function handler(
     const email = req.body?.email || ''
     const storyPath = req.body?.storyPath || ''
 
-    try {
-      const response = await fetch(
-        'https://hooks.slack.com/workflows/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',
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email,
-            storyUrl: `${req.headers.origin}${storyPath}`,
-          }),
-          cache: 'default',
-        }
-      )
-
-      res.status(200).send(response)
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (err: any) {
-      res.status(err.statusCode || 500).json(err.message)
-    }
+    fetch(
+      'https://hooks.slack.com/workflows/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',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          storyUrl: `${req.headers.origin}${storyPath}`,
+        }),
+        cache: 'default',
+      }
+    )
+      .then((response) => {
+        res.status(200).send(response)
+      })
+      .catch((err) => {
+        res.status(err.statusCode || 500).json(err.message)
+      })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method Not Allowed')
