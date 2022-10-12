78be7cd069c3ac66c715437a844fd7abf18666d2
Nick Hellemans <NickHellemans93@gmail.com>
2022-10-12 17:20:11 +0200

    chore(demo): demo page

---
+++ pages/api/arise/start-pathway-session.tsx
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+
+import { type StartHostedPathwaySessionPayload } from '@/types/generated/api.types'
+
+const AWELL_API_ENDPOINT =
+  'https://api.awellhealth.com/orchestration/m2m/graphql'
+const PATHWAY_DEFINITION_ID = '-XRz8Nhlqe54'
+const ARISE_API_KEY = 'LfbozO0FY51Yy6PYI8UpFcxBQxgQ1YW9'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const body = JSON.stringify({
+        query: `
+        mutation StartHostedPathwaySession(
+          $input: StartHostedPathwaySessionInput!,
+        ) {
+          startHostedPathwaySession(input: $input) {
+            session_id
+            session_url
+            stakeholder {
+              type
+              name
+            }
+            pathway_id
+          }
+        }`,
+        variables: {
+          input: {
+            pathway_definition_id: PATHWAY_DEFINITION_ID,
+            success_url: `${req.headers.origin}/demos/arise/?success=true`,
+            cancel_url: `${req.headers.origin}/demos/arise/?canceled=true`,
+          },
+        },
+      })
+
+      const session = await fetch(AWELL_API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          apiKey: ARISE_API_KEY,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body,
+        cache: 'default',
+      })
+        .then((response) => response.json())
+        .then(
+          (response) =>
+            response.data
+              .startHostedPathwaySession as StartHostedPathwaySessionPayload
+        )
+
+      res.status(200).send({ sessionUrl: session.session_url })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      res.status(err.statusCode || 500).json(err.message)
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
---
+++ pages/demos/arise/index.tsx
@@ -0,0 +1,145 @@
+/* eslint-disable @next/next/no-img-element */
+import { CheckIcon, ExclamationIcon } from '@heroicons/react/outline'
+import { ReactNode, useEffect, useState } from 'react'
+
+import { DemoLayout } from '@/components/Layouts/DemoLayout'
+
+export default function HostedPathwayStory() {
+  const [sessionStatus, setSessionStatus] = useState('')
+
+  const onStartSession = async () => {
+    const data = await fetch('/api/arise/start-pathway-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => res.json())
+
+    window.location.href = data?.sessionUrl || ''
+  }
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search)
+
+    if (query.get('success')) {
+      setSessionStatus('success')
+    }
+
+    if (query.get('canceled')) {
+      setSessionStatus('canceled')
+    }
+  }, [])
+
+  if (sessionStatus === 'success') {
+    return (
+      <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+        <div className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden transform transition-all sm:my-8 sm:max-w-md sm:w-full sm:p-6">
+          <div>
+            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+              <CheckIcon
+                className="h-6 w-6 text-green-600"
+                aria-hidden="true"
+              />
+            </div>
+            <div className="mt-3 text-center sm:mt-5">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Session completed
+              </h3>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Your hosted pathway session has been completed.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5 sm:mt-6">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-slate-600 text-base font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 sm:text-sm"
+              onClick={() => {
+                setSessionStatus('')
+              }}
+            >
+              Create a new session
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  if (sessionStatus === 'canceled') {
+    return (
+      <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+        <div className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden transform transition-all sm:my-8 sm:max-w-md sm:w-full sm:p-6">
+          <div>
+            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+              <ExclamationIcon
+                className="h-6 w-6 text-red-600"
+                aria-hidden="true"
+              />
+            </div>
+            <div className="mt-3 text-center sm:mt-5">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Session canceled
+              </h3>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Your hosted pathway session was canceled. This is probably due
+                  to the session being expired.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5 sm:mt-6">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-slate-600 text-base font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 sm:text-sm"
+              onClick={() => {
+                setSessionStatus('')
+              }}
+            >
+              Create a new session
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <form className="text-center">
+      <div className="mx-auto text-center mb-12">
+        <img
+          src="https://uploads-ssl.webflow.com/62a9fa715fba1413c17b2be3/62cb31feb008c03fd1f34aed_lockup.svg"
+          width="120"
+          className="mx-auto"
+          alt="Arise"
+        />
+      </div>
+      <button
+        type="button"
+        onClick={() => onStartSession()}
+        className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-color-slate-200 disabled:cursor-not-allowed`}
+        style={{ backgroundColor: `#3a0eed` }}
+      >
+        Get Started
+      </button>
+    </form>
+  )
+}
+
+HostedPathwayStory.getLayout = function getLayout(page: ReactNode) {
+  return (
+    <DemoLayout
+      title="Arise Hosted Pathway Demo"
+      slug="arise-hosted-pathway"
+      description="Send your clients to an Awell hosted page to complete an onboarding flow or simple pathway."
+      docsUrl="#"
+      codeUrl="#"
+      browserUrl="https://wearise.com/"
+    >
+      {page}
+    </DemoLayout>
+  )
+}
