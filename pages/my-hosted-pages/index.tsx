0439e64f0bdc9d9e9b24a125e35edd195a6f1b14
Nick Hellemans <NickHellemans93@gmail.com>
2022-11-24 13:35:17 +0100

    chore: seo

--- pages/my-hosted-pages/index.tsx
+++ pages/my-hosted-pages/index.tsx
@@ -1,9 +1,18 @@
+import Head from 'next/head'
+
 import { CreateHostedPages } from '@/components/CreateHostedPages'
 import { CreateHostedPagesProvider } from '@/components/CreateHostedPages/context'
 
 export default function MyHostedPages() {
   return (
     <CreateHostedPagesProvider>
+      <Head>
+        <title>Create your hosted page</title>
+        <meta
+          name="description"
+          content="Get started quickly with Awell by creating your own hosted pages session."
+        />
+      </Head>
       <CreateHostedPages />
     </CreateHostedPagesProvider>
   )
