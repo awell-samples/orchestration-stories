1436d2833b790d1686a6271a50a073e31c017f09
Nick Hellemans <NickHellemans93@gmail.com>
2022-12-05 23:49:11 +0100

    style: some rephrsing

--- pages/demos/cityblock/index.tsx
+++ pages/demos/cityblock/index.tsx
@@ -50,9 +50,10 @@ export default function CityBlockFormLibraryDemo() {
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Forms</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all form elements in your care flow.
+          <h1 className="text-2xl font-semibold text-gray-900">Forms</h1>
+          <p className="mt-2 text-base text-gray-700">
+            A list of all form elements in the last published version of your
+            care flow.
           </p>
         </div>
       </div>
